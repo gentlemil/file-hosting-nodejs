e1805a07247920e90a6f0b084fe065d2c8e3da4d
Milosz Bukala <milosz.bukala@giraffestudio.pl>
2024-03-04 13:31:49 +0100

    create post method to upload files, error handling

--- app/app.js
+++ app/app.js
@@ -1,24 +1,57 @@
-const express = require('express');
-const path = require('path');
-const app = express();
-const port = 3000;
+const express = require('express')
+const path = require('path')
+const app = express()
+const port = 3000
+const multer = require('multer')
 
-app.set('view engine', 'ejs');
-app.set('views', path.join(__dirname + '/../views'));
-app.use(express.static('public'));
+app.set('view engine', 'ejs')
+app.set('views', path.join(__dirname + '/../views'))
+app.use(express.static('public'))
 
+const storage = multer.diskStorage({
+  destination(req, file, cb) {
+    cb(null, 'public/uploads/')
+  },
+  filename(req, file, cb) {
+    const name = `${Date.now()}-${file.originalname}`
+    cb(null, name)
+  },
+})
 
+const fileFilter = (req, file, cb) => {
+  const whitelist = ['image/png']
+
+  if (whitelist.includes(file.mimetype)) {
+    cb(null, true)
+  } else {
+    cb(new Error('Niedozwolone rozszerzenie. Dozwolone jedynie: .png'))
+  }
+}
 
 app.get('/', (req, res) => {
-  res.render('home');
-});
+  res.render('home')
+})
 
-app.post('/', (req, res) => {
-  //
-});
-
-
+app.post(
+  '/',
+  multer({ storage, fileFilter }).single('file'),
+  (err, req, res, next) => {
+    if (err) {
+      console.log('#########')
+      console.log(err)
+      return res.render('home', {
+        error: err,
+      })
+    }
+    next()
+  },
+  (req, res) => {
+    res.render('home', {
+      fileURL: req.headers.host + '/uploads/' + req.file.filename,
+    })
+  }
+)
 
 app.listen(port, () => {
-  console.log(`Server uruchomiony na porcie: ${port}`);
-});
+  console.log(`Server uruchomiony na porcie: ${port}`)
+})
